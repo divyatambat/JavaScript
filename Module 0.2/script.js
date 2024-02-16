156982dd667c9a0e2f77534cfe7eddc0ff9f0a4f
Divya Tambat <tambat.divya06@gmail.com>
2024-02-16 15:31:37 +0530

    Javascript Assignment - Module 0.2

---
+++ Module 0.2/script.js
@@ -0,0 +1,54 @@
+// a = "divya";
+// console.log(typeof a)
+// console.log(a);
+// a = 6;
+// console.log(typeof a)
+// console.log(a);
+// a = false;
+// console.log(a);
+
+// let a = "divya";
+// console.log(typeof a)
+// console.log(a);
+// let a = 6;
+// console.log(typeof a)
+// console.log(a);
+// a = false;
+// let console.log(a);
+
+// const a = "divya";
+// console.log(typeof a)
+// console.log(a);
+// const a = 6;
+// console.log(typeof a)
+// console.log(a);
+// const a = false;
+// console.log(typeof Name)
+// console.log(a);
+
+// var array = [1, "divya", false];
+// console.log(typeof array)
+
+// array.push(2);
+// console.log(array);
+
+// array.pop();
+// console.log(array);
+
+// let array = [1, "divya", false];
+// console.log(typeof array)
+
+// array.push(2);
+// console.log(array);
+
+// array.pop();
+// console.log(array);
+
+const array = [1, "divya", false];
+console.log(typeof array)
+
+array.push(2);
+console.log(array);
+
+array.pop();
+console.log(array);
