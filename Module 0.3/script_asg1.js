d68cda6e0d7a922edeb51ca743d693344444b2b4
Divya Tambat <tambat.divya06@gmail.com>
2024-02-16 16:32:35 +0530

    JavaScript Assignment - Module 0.3

---
+++ Module 0.3/script_asg1.js
@@ -0,0 +1,11 @@
+let age = 25;
+
+if(age <= 12){
+    console.log("child!");
+}else if(age <= 18 && age >= 13){
+    console.log("teen!");
+}else if(age > 18) {
+    console.log("adult!");
+}else{
+    console.log("invalid age!")
+}
---
+++ Module 0.3/script_asg2.js
@@ -0,0 +1,16 @@
+let age = 25;
+
+switch(true) {
+    case (age <= 12):
+        console.log("child!");
+        break;
+    case (age <= 18 && age >= 13):
+        console.log("teen!");
+        break;
+    case (age > 18):
+        console.log("adult!");
+        break;
+    default:
+        console.log("invalid age!")
+        break;
+}
